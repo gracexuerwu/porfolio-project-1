1e2014b5eba2cf98b8b2457e8a95af0a451e9962
gracexuerwu <grace.xuer.wu@gmail.com>
2021-11-01 15:18:05 +0100

    Import react navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import Navbar from './Navbar';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from "./components/NavbarComp";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NavbarComp />
     </div>
   );
 }
