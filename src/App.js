a6c228a942198bd3459838e4844d25fa5af08567
gracexuerwu <grace.xuer.wu@gmail.com>
2021-11-01 16:22:23 +0100

    Hero

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavbarComp />
-      <HeroSection />
+      <HeroComp />
     </div>
   );
 }
--- src/components/HeroComp.js
+++ src/components/HeroComp.js
@@ -1,21 +1,26 @@
 import React from "react";
+import "../styles/HeroComp.css";
+import EyesLogo from "../images/eye-hand-drawn-variant.png";
+import CrayonLogo from "../images/crayon-hand-drawn-toy.png";
+import TerminalLogo from "../images/terminal-hand-drawn-symbol.png";
+import MountainLogo from "../images/mountain-landscape-with-trees-and-sun.png";
 
 export default function HeroComp() {
     return (
         <div>
             <div className="hero">
                 <h1>
-                    Inqusitive <img src="images/eye-hand-drawn-variant.png" width="40px" />
+                    Inqusitive <img src={EyesLogo} width="40px" alt="" />
                     <span className="monospace-font">multidisplinary designer</span>
-                    <img src="images/crayon-hand-drawn-toy.png" width="40px" />
+                    <img src={CrayonLogo} width="40px" alt="" />
                     <span className="monospace-font-no-highlight">&</span>
                     <br />
                     <span className="monospace-font">developer</span>
-                    <img src="images/terminal-hand-drawn-symbol.png" width="40px" />
+                    <img src={TerminalLogo} width="40px" alt="" />
                     based in Switzerland
                     <img
-                        src="images/mountain-landscape-with-trees-and-sun.png"
-                        width="45px"
+                        src={MountainLogo}
+                        width="45px" alt=""
                     />
                     with a focus on <span className="monospace-font">Design Management</span>.
                     <span className="smiley-font">b</span>
